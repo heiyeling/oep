a1803071027d335b905b004e31b273d5c42b3f89
luoye0604 <luoye0604@luoye>
2017-09-21 15:35:01 +0800

    user exam scoreofuser 的 model

---
+++ src/main/java/com/zr/model/exam.java
@@ -0,0 +1,47 @@
+package com.zr.model;
+
+public class exam {
+	private int e_id;
+	private String e_name;
+	private String e_starttime;
+	private String e_endtime;
+	private String e_state; 
+	private int e_total;
+	public int getE_id() {
+		return e_id;
+	}
+	public void setE_id(int e_id) {
+		this.e_id = e_id;
+	}
+	public String getE_name() {
+		return e_name;
+	}
+	public void setE_name(String e_name) {
+		this.e_name = e_name;
+	}
+	public String getE_starttime() {
+		return e_starttime;
+	}
+	public void setE_starttime(String e_starttime) {
+		this.e_starttime = e_starttime;
+	}
+	public String getE_endtime() {
+		return e_endtime;
+	}
+	public void setE_endtime(String e_endtime) {
+		this.e_endtime = e_endtime;
+	}
+	public String getE_state() {
+		return e_state;
+	}
+	public void setE_state(String e_state) {
+		this.e_state = e_state;
+	}
+	public int getE_total() {
+		return e_total;
+	}
+	public void setE_total(int e_total) {
+		this.e_total = e_total;
+	}
+	
+}
---
+++ src/main/java/com/zr/model/scoreofuser.java
@@ -0,0 +1,26 @@
+package com.zr.model;
+
+public class scoreofuser {
+	private int u_id;
+	private int e_id;
+	private int score;
+	public int getU_id() {
+		return u_id;
+	}
+	public void setU_id(int u_id) {
+		this.u_id = u_id;
+	}
+	public int getE_id() {
+		return e_id;
+	}
+	public void setE_id(int e_id) {
+		this.e_id = e_id;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+}
