546914ddd5d9756f98b0e5edfce102455ebac32f
zjx <476183771@qq.com>
2017-09-21 15:44:21 +0800

    model

---
+++ src/main/java/com/zr/model/Question.java
@@ -0,0 +1,34 @@
+package com.zr.model;
+
+public class Question {
+
+	private int q_id;
+	private int t_id;
+	private String q_content;
+	private String q_answer;
+	public int getQ_id() {
+		return q_id;
+	}
+	public void setQ_id(int q_id) {
+		this.q_id = q_id;
+	}
+	public int getT_id() {
+		return t_id;
+	}
+	public void setT_id(int t_id) {
+		this.t_id = t_id;
+	}
+	public String getQ_content() {
+		return q_content;
+	}
+	public void setQ_content(String q_content) {
+		this.q_content = q_content;
+	}
+	public String getQ_answer() {
+		return q_answer;
+	}
+	public void setQ_answer(String q_answer) {
+		this.q_answer = q_answer;
+	}
+	
+}
