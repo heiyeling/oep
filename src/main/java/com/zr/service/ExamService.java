59b96c84bc97c35a18e1f8e3ebf325a9ce6602b5
kramer <qkssk1711@163.com>
2017-09-22 11:49:34 +0800

    完成 1.考试查询 2.删除考试
    by qk

---
+++ src/main/java/com/zr/service/ExamService.java
@@ -0,0 +1,22 @@
+package com.zr.service;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+public interface ExamService {
+	/**
+	 * 根据关键词查询考试
+	 * @param key		关键字
+	 * @param currentPage	当前页码
+	 * @param pageSize		每页行数
+	 * @return	考试信息的json对象(total:总数,row:[(examName：考试名,examId:考试id,examTime:考试时间,examState：考试状态)])
+	 */
+	public JSONObject getExamByKey(String key,int currentPage,int pageSize);
+
+	/**
+	 * 根据id[]批量删除考试
+	 * @param examIds
+	 * @return	删除成功返回true
+	 */
+	public boolean deleteExamByIds(int[] examIds);
+}
---
+++ src/main/java/com/zr/service/impl/ExamServiceImpl.java
@@ -0,0 +1,45 @@
+package com.zr.service.impl;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.zr.dao.ExamDao;
+import com.zr.dao.impl.ExamDaoImpl;
+import com.zr.model.Exam;
+import com.zr.service.ExamService;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+public class ExamServiceImpl implements ExamService {
+	ExamDao examDao = new ExamDaoImpl();
+
+	@Override
+	public JSONObject getExamByKey(String key, int currentPage, int pageSize) {
+		JSONObject json = new JSONObject();
+		List<Exam> examList = new LinkedList<>();
+		//count是记录总数即json中的total
+		int count = examDao.getExamByKey(key, (currentPage - 1) * pageSize, pageSize, examList);
+		json.put("total", count);
+		JSONArray examsJson = new JSONArray();
+		for (Exam ee : examList) {
+			//rows数组
+			JSONObject exam = new JSONObject();
+			exam.put("examId", ee.getE_id());
+			exam.put("examName", ee.getE_name());
+			int length = ee.getE_endtime().length();
+			exam.put("examTime", ee.getE_starttime()+" - "+ee.getE_endtime().substring(length-8, length));
+			exam.put("examTotal", ee.getE_total());
+			exam.put("examState", ee.getE_state());
+			examsJson.add(exam);
+		}
+		json.put("rows", examsJson);
+		return json;
+	}
+
+	@Override
+	public boolean deleteExamByIds(int[] examIds) {
+		return examDao.deleteByIds(examIds);
+	}
+
+}
